50aa7c72af4ff13dc18c3f02ffdc79203ab9e9d8
Fred Banda <fredcbanda@hotmail.com>
2024-03-02 11:46:42 +0200

    experience

--- src/pages/about.js
+++ src/pages/about.js
@@ -96,7 +96,7 @@ const About = () => {
 
           <div className="flex flex-col items-end justify-center xl:items-center ">
             <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-            <AnimatedNumbers value={4} /> +
+            <AnimatedNumbers value={10} /> +
             </span>
             <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light
             xl:text-center md:text-l sm:text-base xs:text-sm">years of experience</h2>
